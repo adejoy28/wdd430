e6b89f2d38d740f5871c47caf05eadbf9d70ee70
John Adebayo <johnayomide31@gmail.com>
2026-03-18 12:59:24 +0100

    fix: update PostgreSQL connection environment variable to prod_POSTGRES_URL

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -6,7 +6,7 @@ import { redirect } from 'next/navigation';
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
 import postgres from 'postgres';
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.prod_POSTGRES_URL!, { ssl: 'require' });
 
 
 const FormSchema = z.object({
